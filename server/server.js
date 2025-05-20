2668ab95cca09f6bc560ff32cc427154b784db5c
az-m <alisonzmills@gmail.com>
2025-05-20 16:07:18 +0100

    feat: added get and post methods to test db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 const app = express();
@@ -16,10 +17,38 @@ app.get("/", function (request, response) {
   response.json({ message: "This is the root route of the API." });
 });
 
-import pg from "pg";
 const db = new pg.Pool({
   connectionString: process.env.DB_URL,
 });
+
+app.get("/squad", async (request, response) => {
+  const query = await db.query(`SELECT * FROM squad`);
+  response.json(query.rows);
+});
+
+app.get("/goalkeepers", async (request, response) => {
+  const query = await db.query(
+    `SELECT * FROM squad WHERE position = 'goalkeeper'`
+  );
+  response.json(query.rows);
+});
+
+app.post("/addPlayer", (request, response) => {
+  const body = request.body;
+  const query = db.query(
+    `INSERT INTO squad (player_name, position, nationality, dob, squad_number) VALUES($1,$2,$3,$4,$5)`,
+    [
+      body.player_name,
+      body.position,
+      body.nationality,
+      body.dob,
+      body.squad_number,
+    ]
+  );
+  response.json(query);
+});
+
+// gets unsplash image data for the env workshop
 
 app.get("/images", async (req, res) => {
   const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=puppy`;
