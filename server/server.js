b5ae1e2467df95e0f1d1f8b6ce9c3f151356e792
az-m <alisonzmills@gmail.com>
2025-05-20 10:58:13 +0100

    chore: added env for db

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(8080, function () {
   console.log("Server is alive and listening on 8080");
 });
 
 app.get("/", function (request, response) {
-  response.json({ message: "This is the root of the API." });
+  response.json({ message: "This is the root route of the API." });
+});
+
+import pg from "pg";
+const db = new pg.Pool({
+  connectionString: process.env.DB_URL,
 });
