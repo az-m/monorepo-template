1fb1fb6bf22139dd8d1936833d2781da62db1ff3
az-m <alisonzmills@gmail.com>
2025-05-21 13:51:05 +0100

    feat: added form to client to add items to db table

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.getElementById("form");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function handleSubmit(event) {
+  event.preventDefault();
 
-setupCounter(document.querySelector('#counter'))
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+
+  const API = import.meta.env.VITE_API_ROOT + "addPlayer";
+
+  fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
+
+  form.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
